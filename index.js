28c73d7da85c6c94ddab6f395fd7848021e8165f
tatukristiani <tatu.pulkkinen@metropolia.fi>
2023-08-08 19:14:32 +0300

    Made some small changes

--- index.js
+++ index.js
@@ -21,37 +21,9 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 // Cross Origin Resource Sharing
-//app.use(cors());
-/* Allowed origins, localhost + netlify
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:4000',
-    'https://keen-cassata-9a91f3.netlify.app'
-];
-*/
+app.use(cors());
 
-// Add Access Control Allow Origin headers (Only Netlify)
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://keen-cassata-9a91f3.netlify.app");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
-
-// Access Control Allow Origin headers (Localhost and Netlify)
-/*
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", ["https://keen-cassata-9a91f3.netlify.app", "http://localhost:3000"]);
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
-*/
-
+// Root to test if ONLINE
 app.use("/", require("./routes/root"));
 
 // Routes for product related endpoints
