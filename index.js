294e5d5a0adfc30813615786b247cdf4e7663def
tatukristiani <tatu.pulkkinen@metropolia.fi>
2023-08-08 18:43:19 +0300

    2nd test of cors change

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 // Cross Origin Resource Sharing
-app.use(cors());
+//app.use(cors());
 app.use(cors(corsOptions)); // Cors options declare what origins are allowed.
 
 app.use("/", require("./routes/root"));
